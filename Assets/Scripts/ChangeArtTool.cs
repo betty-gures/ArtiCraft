762c97efa1f7e89260eab7c0546b1253da805b76
Betul Gures <fatma.betul.gures@gmail.com>
2022-10-24 23:52:51 +0300

    Progress Monday

---
+++ Assets/Scripts/ChangeArtTool.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeArtTool : MonoBehaviour
+{
+    [SerializeField] private MeshFilter _meshRendererTools;
+    [SerializeField] private Mesh[] _modelsToChange;
+
+    private int _currentModelIndex = 0;
+
+    public void ChangeTool()
+    {
+        _meshRendererTools.mesh = _modelsToChange[_currentModelIndex];
+        _currentModelIndex++;
+        if (_currentModelIndex > _modelsToChange.Length)
+        {
+            _currentModelIndex = 0;
+        }
+    }
+}
--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -9,20 +9,5 @@ public class ScoreCounter : MonoBehaviour
     [SerializeField] ResetCanPositions _resetCanPositions;
     [SerializeField] private TextMeshProUGUI _scoreText;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag != "Can")
-            return;
-        
-        else if (other.gameObject.tag == "Can");
-        {
-            _resetCanPositions._score++;
-            _scoreText.text = _resetCanPositions._score.ToString();
-            
-            if (_resetCanPositions._score >= 6)
-            {
-                _scoreText.text = "You Won";
-            }
-        }
-    }
+    
 }
