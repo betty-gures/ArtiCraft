8fd075d9e3922ffab5cf48cae4eade13a484ce28
Betul Gures <fatma.betul.gures@gmail.com>
2022-10-26 23:05:57 +0300

    Last Changes (to be continued...)

--- Assets/Scripts/ChangeArtTool.cs
+++ Assets/Scripts/ChangeArtTool.cs
@@ -6,27 +6,64 @@ using UnityEngine;
 public class ChangeArtTool : MonoBehaviour
 {
     [SerializeField] private MeshFilter _meshRendererTools;
-    [SerializeField] private GameObject[] _gameObjects;
-    
-    List<Mesh> _modelsToChange = new List<Mesh>();
+    [SerializeField] private Mesh[] _modelsToChange;
+    [SerializeField] private Material[] _materials;
+    [SerializeField] private GameObject[] _tryGrabMesh;
+
+    private MeshRenderer _meshRenderer;
+
+    public List<MeshFilter> _trygrabMeshRenderer = new List<MeshFilter>();
     private int _currentModelIndex = 0;
 
     private void Start()
     {
-        foreach (var gameObject in _gameObjects)
+        _meshRenderer = GetComponent<MeshRenderer>();
+        foreach (var gameObject in _tryGrabMesh)
         {
-            var mesh= gameObject.GetComponent<Mesh>();
-            _modelsToChange.Add(mesh);
+            var mesh= gameObject.GetComponent<MeshRenderer>();
+            //_trygrabMeshRenderer.Add(mesh);
         }
+        
     }
+
 
     public void ChangeTool()
     {
-        _meshRendererTools.mesh = _modelsToChange[_currentModelIndex];
+        //_meshRendererTools.mesh = _trygrabMeshRenderer[0].;
         _currentModelIndex++;
-        if (_currentModelIndex >= _modelsToChange.Count)
+        if (_currentModelIndex >= _modelsToChange.Length)
         {
             _currentModelIndex = 0;
         }
     }
+
+    public void ChangeMaterial1()
+    {
+        _meshRenderer.material = _materials[0];
+    }
+    public void ChangeMaterial2()
+    {
+        _meshRenderer.material = _materials[1];
+    }
+    public void ChangeMaterial3()
+    {
+        _meshRenderer.material = _materials[2];
+    }
+    public void ChangeMaterial4()
+        {
+            _meshRenderer.material = _materials[3];
+        }
+    public void ChangeMaterial5()
+        {
+            _meshRenderer.material = _materials[4];
+        }
+    public void ChangeMaterial6()
+        {
+            _meshRenderer.material = _materials[5];
+        }
+    public void ChangeMaterial7()
+        {
+            _meshRenderer.material = _materials[6];
+        }
+   
 }
